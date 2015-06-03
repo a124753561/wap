67b427ac8e546d70fd7851d6c4e67b863e7d106b
chenlf <fjchenlongfei@163.com>
2015-06-03 16:26:10 +0800

    ignore

--- static/js/common.js
+++ static/js/common.js
@@ -1,25 +1,36 @@
-(function($) {
-	WapJS = function() {
-		// css 动画
-		this.doScale = function(element, scaleAction) {
-			$(element).addClass(scaleAction).one($.AMUI.support.animation.end, function() {
-				$(element).removeClass(scaleAction);
-			});
-		}
-		// 遮罩层
-		this.modalFade = function(content, fade) {
-			if ($("#wap-modal-pannel").lenght == 0) {
-				var modalHtml = '<div class="am-modal am-modal-no-btn" tabindex="-1" id="wap-modal-pannel"><div class="wap-noicon-tips"><div class="am-modal-bd"></div></div></div>';
-				$("body").append(modalHtml);
-			}
-			$(".am-modal-bd").text(content);
-			$("#wap-modal-pannel").modal("open");
-			if (fade) {
-				setTimeout(function() {
-					$("#wap-modal-pannel").modal("close");
-				}, fade);
-			}
-		}
-	}
+(function ($) {
+    WapJS = function () {
+        // css 动画
+        this.doScale = function (element, scaleAction) {
+            $(element).addClass(scaleAction).one($.AMUI.support.animation.end, function () {
+                $(element).removeClass(scaleAction);
+            });
+        }
+        // 遮罩层
+        this.modalFade = function (content, fade) {
+            if ($("#wap-modal-pannel").lenght == 0) {
+                var modalHtml = '<div class="am-modal am-modal-no-btn" tabindex="-1" id="wap-modal-pannel"><div class="wap-noicon-tips"><div class="am-modal-bd"></div></div></div>';
+                $("body").append(modalHtml);
+            }
+            $(".am-modal-bd").text(content);
+            $("#wap-modal-pannel").modal("open");
+            if (fade) {
+                setTimeout(function () {
+                    $("#wap-modal-pannel").modal("close");
+                }, fade);
+            }
+        }
+    }
+
 
 })(window.jQuery)
+
+
+function lazyloadimg(){
+    if (typeof($(".lazy")) != "undefined" && $(".lazy").length > 0) {
+        $(".lazy").lazyload({
+            effect: "fadeIn",
+            threshold: 200
+        });
+    }
+}
--- static/js/iscrollEvents.js
+++ static/js/iscrollEvents.js
@@ -1,132 +1,108 @@
-(function($) {
-	EventsList = function(e1, e2, options) {
-		var $main = $(e1);
-		var $list = $main.find('#events-list');
-		var $pullDown = $main.find('#pull-down');
-		var $pullDownLabel = $main.find('#pull-down-label');
-		var $pullUp = $main.find('#pull-up');
-		var topOffset = -$pullDown.outerHeight();
+var  data=[{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"},{"image":"http://img5.douban.com/lpic/o636459.jpg"}];
+var EventsList = function (e1, e2, options) {
+	var $main = e1;
+	var $list = $main.find('#events-list');
+	var $pullDown = $main.find('#pull-down');
+	var $pullDownLabel = $main.find('#pull-down-label');
+	var $pullUp = $main.find('#pull-up');
+	var topOffset = -$pullDown.outerHeight();
 
-		this.compiler = Handlebars.compile($(e2).html());
-		this.prev = this.next = this.start = options.params.pageNumber;
-		this.total = null;
+	this.compiler = Handlebars.compile(e2.html());
+	this.prev = this.next = this.start = options.params.start;
+	this.total = 1000;
 
-		this.getURL = function(params) {
-			var queries = [];
-			for ( var key in params) {
-				if (key !== 'pageNumber') {
-					queries.push(key + '=' + params[key]);
-				}
+
+	this.renderList = function (start, type) {
+		var _this = this;
+		var $el = $pullDown;
+
+		if (type === 'load') {
+			$el = $pullUp;
+		}
+		_this.setLoading($el);
+		var html = _this.compiler(data);
+
+		if (type === 'refresh') {
+			$list.html(html);
+		} else if (type === 'load') {
+			$list.append(html);
+		} else {
+			$list.html(html);
+		}
+
+		// refresh iScroll
+		setTimeout(function () {
+			_this.resetLoading($el);
+			_this.iScroll.refresh();
+		}, 100);
+		if (type !== 'load') {
+			_this.iScroll.scrollTo(0, topOffset, 800, $.AMUI.iScroll.utils.circular);
+		}
+		lazyloadimg();
+	};
+
+	this.setLoading = function ($el) {
+		$el.addClass('loading');
+	};
+
+	this.resetLoading = function ($el) {
+		$el.removeClass('loading');
+	};
+
+	this.init = function () {
+		var myScroll = this.iScroll = new $.AMUI.iScroll('#' + e1.attr("id"), {});
+		// myScroll.scrollTo(0, topOffset);
+		var _this = this;
+		var pullFormTop = false;
+		var pullStart;
+
+		this.renderList(options.params.start);
+
+		myScroll.on('scrollStart', function () {
+			if (this.y >= topOffset) {
+				pullFormTop = true;
 			}
-			queries.push('pageNumber=');
-			return options.api + '?' + queries.join('&');
-		};
 
-		this.renderList = function(start, type) {
-			var _this = this;
-			var $el = $pullDown;
+			pullStart = this.y;
+			// console.log(this);
+		});
 
-			if (type === 'load') {
-				$el = $pullUp;
+		myScroll.on('scrollEnd', function () {
+			if (pullFormTop && this.directionY === -1) {
+				_this.handlePullDown();
 			}
+			pullFormTop = false;
 
-			$.ajax({
-				url : this.URL + start,
-				type : "get",
-				dataType : "json",
-				success : function(data) {
-					console.log(data);
-					_this.total = 2000;
-					var html = _this.compiler(data);
-					if (type === 'refresh') {
-						$list.children('li').first().before(html);
-					} else if (type === 'load') {
-						$list.append(html);
-					} else {
-						$list.html(html);
-					}
-					// refresh iScroll
-					setTimeout(function() {
-						_this.iScroll.refresh();
-					}, 100);
-					_this.resetLoading($el);
-					if (type !== 'load') {
-						_this.iScroll.scrollTo(0, topOffset, 800, $.AMUI.iScroll.utils.circular);
-					}
-				},
-				error: function(XMLHttpRequest, textStatus, errorThrown){
-					 console.log(XMLHttpRequest.status);
-                        console.log(XMLHttpRequest.readyState);
-                        console.log(textStatus);
-					}
-			});
-		};
-
-		this.setLoading = function($el) {
-			$el.addClass('loading');
-		};
-
-		this.resetLoading = function($el) {
-			$el.removeClass('loading');
-		};
-
-		this.init = function() {
-			var myScroll = this.iScroll = new $.AMUI.iScroll(e1, {});
-			// myScroll.scrollTo(0, topOffset);
-			var _this = this;
-			var pullFormTop = false;
-			var pullStart;
-
-			this.URL = this.getURL(options.params);
-			this.renderList(options.params.pageNumber);
-
-			myScroll.on('scrollStart', function() {
-				if (this.y >= topOffset) {
-					pullFormTop = true;
-				}
-
-				pullStart = this.y;
-				// console.log(this);
-			});
-
-			myScroll.on('scrollEnd', function() {
-				if (pullFormTop && this.directionY === -1) {
-					_this.handlePullDown();
-				}
-				pullFormTop = false;
-
-				// pull up to load more
-				if (pullStart === this.y && (this.directionY === 1)) {
-					_this.handlePullUp();
-				}
-			});
-		};
-
-		this.handlePullDown = function() {
-			console.log('handle pull down');
-			if (this.prev > 0) {
-				this.setLoading($pullDown);
-				this.prev -= options.params.pageSize;
-				this.renderList(this.prev, 'refresh');
-			} else {
-				console.log('别刷了，没有了');
+			// pull up to load more
+			if (pullStart === this.y && (this.directionY === 1)) {
+				_this.handlePullUp();
 			}
-		};
+		});
+	};
 
-		this.handlePullUp = function() {
-			console.log('handle pull up');
-			if (this.next < this.total) {
-				this.setLoading($pullUp);
-				this.next += options.params.pageSize;
-				this.renderList(this.next, 'load');
-			} else {
-				console.log(this.next);
-				// this.iScroll.scrollTo(0, topOffset);
-			}
+	this.handlePullDown = function () {
+		console.log('handle pull down');
+		if (this.prev > 0) {
+			this.setLoading($pullDown);
+			this.prev = options.params.start;
+			this.renderList(this.prev, 'refresh');
+		} else {
+			console.log('别刷了，没有了');
 		}
 	};
 
-	document.addEventListener('touchmove', function(e) {
-		e.preventDefault();
-	}, false);
-})(window.jQuery);
+	this.handlePullUp = function () {
+		console.log('handle pull up');
+		if (this.next < this.total) {
+			this.setLoading($pullUp);
+			this.next += options.params.count;
+			this.renderList(this.next, 'load');
+		} else {
+			console.log(this.next);
+			// this.iScroll.scrollTo(0, topOffset);
+		}
+	}
+};
+document.addEventListener('touchmove', function (e) {
+	e.preventDefault();
+}, false);
